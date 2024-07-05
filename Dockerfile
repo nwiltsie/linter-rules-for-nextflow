86ae17931eea0e7f071d7ff21b00a0f8960f48fb
Nicholas Wiltsie <nwiltsie@mednet.ucla.edu>
2024-07-05 14:18:49 -0700

    Use non-Amazon rules, emit JSON

--- Dockerfile
+++ Dockerfile
@@ -19,5 +19,4 @@ RUN wget https://repo1.maven.org/maven2/org/codenarc/CodeNarc/3.3.0/CodeNarc-3.3
     wget https://repo1.maven.org/maven2/org/slf4j/slf4j-api/1.7.36/slf4j-api-1.7.36.jar && \
     wget https://repo1.maven.org/maven2/org/slf4j/slf4j-simple/1.7.36/slf4j-simple-1.7.36.jar
 
-ENV ruleset "healthomics"
-ENTRYPOINT bash check.sh ${ruleset}
+ENTRYPOINT ["bash", "check.sh"]
--- scripts/check.sh
+++ scripts/check.sh
@@ -1,12 +1,8 @@
 #! /bin/bash
 
-RULESET="$1"
-echo  "Checking with ruleset: ${RULESET:=healthomics}"
-
-java  -Dorg.slf4j.simpleLogger.defaultLogLevel=error \
+java -Dorg.slf4j.simpleLogger.defaultLogLevel=error \
   -classpath linter-rules.jar:CodeNarc-3.3.0-all.jar:slf4j-api-1.7.36.jar:slf4j-simple-1.7.36.jar \
   org.codenarc.CodeNarc \
-  -report=text:stdout \
-  -rulesetfiles=rulesets/"${RULESET}".xml \
-  -basedir=/data \
-  -includes=**/*.nf
+  -report=json:stdout \
+  -rulesetfiles=rulesets/general.xml \
+  "$@"
